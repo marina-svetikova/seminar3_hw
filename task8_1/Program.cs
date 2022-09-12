3c5a96f899f3ac7b82f122e48ab99ed57a91f283
Svetikova Marina <m-golovleva@yandex.ru>
2022-09-12 17:32:22 +0500

    task8_1

---
+++ task8_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите положительное число");
+int number = Convert.ToInt32 (Console.ReadLine());
+int numberA = 2;
+
+while (numberA <= number)
+{
+  Console.WriteLine (numberA + " ");
+  numberA +=2;
+}
