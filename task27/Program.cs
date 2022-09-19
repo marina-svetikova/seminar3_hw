00aae1ce667ec99142d54dd2d24b341163dd0b04
Svetikova Marina <m-golovleva@yandex.ru>
2022-09-19 17:00:57 +0500

    task27

---
+++ task27/Program.cs
@@ -0,0 +1,31 @@
+﻿// // Напишите программу, которая принимает на вход число и выдаёт
+//  сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int ReadNumber (string messenge)
+{
+    Console.WriteLine(messenge);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int numberA = ReadNumber ("Ведите положительное число:");
+if (numberA>0)
+{ 
+    int TempNumber = numberA;
+    int sum = 0; //Сумма цифр в числе
+    int digit = 0;
+    while(TempNumber!=0)
+    {
+        digit = TempNumber%10;
+        TempNumber = TempNumber/10;
+        sum = sum + digit;
+    }
+    Console.WriteLine($"Сумма цифр в числе = {sum}");
+}
+else
+{
+    Console.WriteLine("Введено число меньше или равное 0");
+}
+
